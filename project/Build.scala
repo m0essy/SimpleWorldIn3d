ad048b3fc0de350a5b7f3c8c1e2ec0a1e86a4643
Gerhard <gs@techtalk.at>
2012-11-22 10:47:02 +0100

    Update dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -7,17 +7,19 @@ object IslandBuild extends Build {
       name := "island-main",
       version := "1.0-SNAPSHOT",
       organization := "org.unlimited",
-      scalaVersion := "2.9.1",
+      scalaVersion := "2.10.0-RC2",
       fork := true,
 
       libraryDependencies ++= Seq(
-        "org.specs2" %% "specs2" % "1.6.1",
+        "org.specs2" %% "specs2" % "1.12.3",
         "org.specs2" %% "specs2-scalaz-core" % "6.0.1" % "test",
-        "org.scala-tools.testing" %% "scalacheck" % "1.9" % "test"
+        "org.scalacheck" %% "scalacheck" % "1.10.0" % "test"
       ),
 
       resolvers ++= Seq("snapshots" at "http://scala-tools.org/repo-snapshots",
-        "releases" at "http://scala-tools.org/repo-releases"),
+        "releases" at "http://scala-tools.org/repo-releases",
+        "Sonatype Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+        "Sonatype Releases" at "http://oss.sonatype.org/content/repositories/releases"),
 
       unmanagedJars in Compile <<= baseDirectory map { base => (base ** "*.jar").classpath }
     ) ++ JMonkeyProject.jmonkeySettings
